272b8446bb187a5cc44cc251985c7f962f97d826
Hassan Ngala <hassan@lorisystems.com>
2018-09-17 16:19:37 +0300

    Add Home Page

---
+++ Django_Project_App_Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hassan_samini/Desktop/Django_Project_App/Django_Project_App_Env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- Django_Simple_User_Project/Django_Simple_User_Project/urls.py
+++ Django_Simple_User_Project/Django_Simple_User_Project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from  Django_User_App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.home,name='home'),
 ]
+
+
+
